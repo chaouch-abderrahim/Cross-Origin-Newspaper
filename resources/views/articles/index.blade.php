546da210d021dcde8c6d17ccdc14247a07721d9a
Abderrahim_chaouch <a2chaouc@enib.fr>
2024-11-30 22:30:14 +0100

    3.3. Ajouter la possibilité de rechercher certains articles

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -5,55 +5,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Liste des articles</title>
-    <!-- Inclure les fichiers CSS de DataTables -->
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
 </head>
 <body>
-    <h1>Liste des articles de Journal Le Monde</h1>
+    <!--J'ai travaillé dès le début avec DataTable, parce que j'ai essayé de travailler challenge par challenge. 
+        Je ne sais pas si j'ai le droit ou pas d'utiliser DataTable.
+    -->
+    <div class="container mt-5">
+        <h1 class="text-center mb-4">Liste des articles </h1>
 
-    @if ($articlesLeMonde->isEmpty())
-        <p>Aucun article disponible.</p>
-    @else
-        <table id="articlesTable" class="display">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Titre</th>
-                    <th>Auteur</th>
-                    <th>Catégorie</th>
-                    <th>Date de publication</th>
-                    <th>Contenu</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($articlesLeMonde as $article)
+        @if ($articlesLeMonde->isEmpty())
+            <p class="text-center">Aucun article disponible.</p>
+        @else
+            <table id="articlesTable" class="table table-success table-striped table-bordered">
+                <thead class="thead-success">
                     <tr>
-                        <td>{{ $article->id }}</td>
-                        <td>{{ $article->title }}</td>
-                        <td>{{ $article->author ?? 'Non spécifié' }}</td>
-                        <td>{{ $article->category ?? 'Non spécifiée' }}</td>
-                        <td>{{ \Carbon\Carbon::parse($article->published_at)->format('d-m-Y H:i') }}</td>
-                        <td>{{ $article->content }}</td>
+                        <th>ID</th>
+                        <th>Titre</th>
+                        <th>Auteur</th>
+                        <th>Catégorie</th>
+                        <th>Date de publication</th>
+                        <th>Contenu</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
-    @endif
+                </thead>
+                <tbody>
+                    @foreach ($articlesLeMonde as $article)
+                        <tr>
+                            <td>{{ $article->id }}</td>
+                            <td>{{ $article->title }}</td>
+                            <td>{{ $article->author ?? 'Non spécifié' }}</td>
+                            <td>{{ $article->category ?? 'Non spécifiée' }}</td>
+                            <td>{{ \Carbon\Carbon::parse($article->published_at)->format('d-m-Y H:i') }}</td>
+                            <td>{{ $article->content }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
+    </div>
 
-    <!-- Inclure les fichiers JavaScript de jQuery et DataTables -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script>
         $(document).ready(function() {
             $('#articlesTable').DataTable({
-                "ordering": true // activation  de filtre 
+                "ordering": true // Activation du tri
             });
-            /*
-            pour filtrer par categorie 
-            $('#categoryFilter').on('change', function() { var category = $(this).val(); table.column(3).search(category).draw(); })
-            */
         });
     </script>
 </body>
 </html>
-
